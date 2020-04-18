034bb513e5aeb401da37b6a215ff79e3f8b38f3e
GusGo <gus.gonzalez.15@gmail.com>
2020-04-18 18:21:33 -0400

    api post route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,8 @@ const db = require("../models");
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
         .then(dbWorkout => {
-            res.json(dbWorkout)
+            // console.log(dbWorkout);
+            res.status(200).json(dbWorkout)
         })
         .catch(err => {
             res.json(err);
@@ -16,10 +17,22 @@ router.put("/api/workouts/:id", (req, res) => {
 
     db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } })
         .then(result => {
-            res.json(result);
+            // console.log(result);
+            res.status(400).json(result);
         })
         .catch(err => {
-            res.status(400).json(err);
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+        .then(workout => {
+            // console.log(workout);
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
         });
 });
 
