6d2ef0e3772c11231d51c98b82ac14ccaf61a393
GusGo <gus.gonzalez.15@gmail.com>
2020-04-18 19:43:40 -0400

    final schema changes including custom methods

--- models/workout.js
+++ models/workout.js
@@ -2,16 +2,45 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: Date,
+    day: {
+        type: Date,
+        default: Date.now()
+    },
     exercises: [{
-        type: String,
-        name: String,
-        duration: Number,
+        type: {
+            type: String,
+            trim: true,
+            required: "Exercise needs a type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Exercise needs a name"
+        },
+        duration: {
+            type: Number,
+            required: "Exercise needs a duration"
+        },
         weight: Number,
         reps: Number,
-        sets: Number
-    }]
+        sets: Number,
+        distance: Number
+    }],
+    totalDuration: Number
 });
+
+WorkoutSchema.methods.setTotalDuration = function () {
+    // console.log("called");
+    let sum = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        sum += this.exercises[i].duration;
+    }
+    this.totalDuration = sum;
+};
+
+// WorkoutSchema.methods.numExercises = function () {
+//     return this.exercises.length;
+// };
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
