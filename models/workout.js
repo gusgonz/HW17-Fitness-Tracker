e5646639c110b1a35d881347312d17a677970e21
GusGo <gus.gonzalez.15@gmail.com>
2020-04-18 17:07:27 -0400

    changed how workout and exercise are associated

--- models/workout.js
+++ models/workout.js
@@ -4,12 +4,8 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     day: Date,
     exercises: [{
-        type: String,
-        name: String,
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
     }]
 });
 
