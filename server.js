ee3ccb1e8b957d7429a098c9d0a5ab50fbfb9ae4
GusGo <gus.gonzalez.15@gmail.com>
2020-04-18 17:06:51 -0400

    using my api routes on server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
-const db = require("./models");
+// const db = require("./models");
 
 const app = express();
 
@@ -14,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(require("./routes/api-routes.js"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
